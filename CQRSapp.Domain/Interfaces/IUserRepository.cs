d65badb1748772dae8b8b9ee95224d71f7792f13
Administrator <Administrator@DESKTOP-TT9EO7T>
2026-07-15 13:14:33 +0800

    added Auth

---
+++ CQRSapp.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSapp.Domain.Interfaces
+{
+    internal class IUserRepository
+    {
+    }
+}
---
+++ CQRSapp.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,48 @@
+﻿using CQRSapp.Application.Auth.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSapp.Infrastructure.Authentication
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(Guid userId,string username) 
+        {
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Secret"]));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, username)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(5),
+                signingCredentials: credentials
+            );
+
+            // Implementation for generating JWT token
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
